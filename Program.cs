f5dc46801f7fd0718649b4f61515f722dff8ea84
Mugunthan <mugunthanraghul56@gmail.com>
2024-11-06 10:26:09 +0530

    Hi

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppThreading;
+
+Console.WriteLine("Hello, World!");
+Thread thread = new Thread(new ThreadStart(OurClass.Display));
+Console.WriteLine("Thread Details");
+Console.WriteLine("Background:\t" + thread.IsBackground);
+Console.WriteLine("Name:\t" +thread.Name);
+Console.WriteLine("State:\t" +thread.ThreadState);
+Console.WriteLine("Is Alive? \t" +thread.IsAlive);
+thread.Start();
